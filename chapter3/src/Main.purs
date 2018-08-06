01992f0e1379c7ced1a0cdbb012b108f8b4b1f8a
Romain Ernandez <romain.ernandez@gmail.com>
2018-08-06 18:25:42 +0200

    inline declaration

--- chapter3/src/Main.purs
+++ chapter3/src/Main.purs
@@ -9,6 +9,10 @@ author = { name: "Phil", interests: ["Functional Programming", "Javascript"]}
 add :: Int -> Int -> Int
 add a b = a + b
 
+otherAdd :: Number -> Number -> Number
+otherAdd = \a b -> a + b
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   logShow $ add 1 2
+  logShow $ otherAdd 1.0 2.1
