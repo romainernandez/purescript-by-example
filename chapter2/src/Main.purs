6140b95a7297622cfa547b808eca4daade48fc43
Romain Ernandez <romain.ernandez@gmail.com>
2018-08-06 17:13:02 +0200

    diagonal + logShow

--- chapter2/src/Main.purs
+++ chapter2/src/Main.purs
@@ -2,8 +2,11 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Math (sqrt)
+
+diagonal w h = sqrt (w * w + h * h)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  logShow (diagonal 4.0 3.0)
