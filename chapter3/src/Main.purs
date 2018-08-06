8e0ac3d8b3688430866e6b148231cca9439d89c8
Romain Ernandez <romain.ernandez@gmail.com>
2018-08-06 18:20:19 +0200

    instantiate object

--- chapter3/src/Main.purs
+++ chapter3/src/Main.purs
@@ -2,8 +2,11 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+
+author = { name: "Phil", interests: ["Functional Programming", "Javascript"]}
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  logShow author.name
+  logShow author.interests
