ee4308c4a2ca267bc9c847a5b54339013d01a9d3
Romain Ernandez <romain.ernandez@gmail.com>
2018-08-06 16:02:06 +0200

    iAmANumber + show

--- chapter1/src/Main.purs
+++ chapter1/src/Main.purs
@@ -4,6 +4,10 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+iAmANumber =
+  let square x = x * x
+  in square 42.0
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  log $ show iAmANumber
