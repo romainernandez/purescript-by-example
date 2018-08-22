20bca8c2fb4aab9c16819048949a31084245a161
Romain Ernandez <romain.ernandez@gmail.com>
2018-08-22 13:50:31 +0200

    stuck with Could not match type List with type List

--- chapter3/src/Data/AddressBook.purs
+++ chapter3/src/Data/AddressBook.purs
@@ -3,6 +3,9 @@ module Data.AddressBook where
 import Prelude
 import Data.List.Lazy.Types (List, cons)
 import Control.Plus (empty)
+import Data.Maybe (Maybe)
+import Data.List (head, filter)
+
 
 type Entry = {
   firstName :: String,
@@ -33,3 +36,9 @@ emptyBook = empty
 
 insertEntry :: Entry -> AddressBook -> AddressBook
 insertEntry  = cons
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName adressBook = head $ filter filterEntry adressBook
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = firstName == entry.firstName && lastName == entry.lastName
