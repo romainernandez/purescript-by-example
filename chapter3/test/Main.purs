d2c60f6959d436e8df9a32fc8be3ca6ffa49f503
Romain Ernandez <romain.ernandez@gmail.com>
2018-08-23 16:31:39 +0200

    moving Main to Test module

--- chapter3/test/Main.purs
+++ chapter3/test/Main.purs
@@ -2,8 +2,54 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Data.AddressBook
+import Data.List (filter, (..))
+
+author = { name: "Phil", interests: ["Functional Programming", "Javascript"]}
+
+add :: Int -> Int -> Int
+add a b = a + b
+
+otherAdd :: Number -> Number -> Number
+otherAdd = \a b -> a + b
+
+indentedAdd :: Int -> Int -> Int
+indentedAdd a b = a
+  + b
+
+exemple :: Int -> Int -> Int -> Int
+exemple x y z = foo + bar
+  where
+    foo = x * y
+    bar = y * z
+
+exempleAdress = {
+  street: "221b Baker St",
+  city: "London",
+  state: "UK"
+} :: Address
+
+exempleEntry = {
+  firstName: "Sherlock",
+  lastName: "Holmes",
+  address: exempleAdress
+}  :: Entry
+
+exempleAdressBook = insertEntry exempleEntry emptyBook :: AddressBook
+
+gte2 :: Int -> Boolean
+gte2 x = x >= 2
+
+add5 :: Number -> Number
+add5 = otherAdd 5.0
+
+mult2 :: Number -> Number
+mult2 x = x * 2.0
+
+add5mult2 :: Number -> Number
+add5mult2 = mult2 <<< add5
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "You should add some tests."
+  logShow $ showEntry exempleEntry
